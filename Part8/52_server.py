8b2e86420fe25002d5cdb20864e7214d6c5469b5
Hajun Park <jbaltop@gmail.com>
2018-02-03 11:29:27 +0900

    fix import name

--- Part8/52_server.py
+++ Part8/52_server.py
@@ -4,7 +4,7 @@ import time
 from flask import Flask, request
 from flask_restful import Resource, Api
 from json import dumps
-from flask.ext.jsonpify import jsonify
+import flask_jsonpify
 
 app = Flask(__name__)
 api = Api(app)
