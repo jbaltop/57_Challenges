549dadf259083f517ef17356860a0c52260e5517
Hajun Park <jbaltop@gmail.com>
2018-03-19 17:33:09 +0900

    Fix typo

--- Part8/52_server.py
+++ Part8/52_server.py
@@ -4,7 +4,7 @@ import time
 from flask import Flask, request
 from flask_restful import Resource, Api
 from json import dumps
-import flask_jsonpify
+import flask_jsonify
 
 app = Flask(__name__)
 api = Api(app)
